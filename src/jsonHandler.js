585116451b095548b8b855b3ce6de5de6163259a
Katy Mollenkopf <kmm5278@rit.edu>
2019-09-24 18:43:51 -0400

    Start assignment

---
+++ src/jsonHandler.js
@@ -0,0 +1,25 @@
+// takes request, response, status code and object to send
+const respondJSON = (request, response, status, object) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+    // send response with json object
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const notFound = (request, response) => {
+  // create error message for response
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  // return a 404 with an error message
+  respondJSON(request, response, 404, responseJSON);
+};
+
+module.exports = {
+  notFound,
+};
--- src/server.js
+++ src/server.js
@@ -1,9 +1,33 @@
 const http = require('http');
+const url = require('url');
+
+const htmlHandler = require('./htmlHandler.js');
+const jsonHandler = require('./jsonHandler.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+const urlStruct = {
+  'GET': {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    notFound: jsonHandler.notFound,
+  },
+  'HEAD': {
+
+  },
+  'POST': {
+
+  },
+};
 const onRequest = (request, response) => {
-  
+  const parsedUrl = url.parse(request.url);
+
+  // check if a handler for the request is implemented
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response);
+  } else {
+    urlStruct[request.method].notFound(request, response);
+  }
 };
 
 http.createServer(onRequest).listen(port);
